c3028b5f28cf58832515db378db87122ea043e51
ggopakumar <51371711+ggopakumar@users.noreply.github.com>
2019-08-16 00:48:27 +0530

    updating the docker build

--- dockerize.bat
+++ dockerize.bat
@@ -1,6 +1,6 @@
-docker build --tag=registry-ms ../registry-ms/
-docker build --tag=product-ms ../product-ms/
-docker build --tag=cart-ms ../cart-ms/
-docker build --tag=etailer-ui ../etailer-ui/
+docker build --tag=registry-ms ../registry/
+docker build --tag=product-ms ../product/
+docker build --tag=cart-ms ../CART/
+docker build --tag=etailer-ui ../Nimbus-shopping-Cart-gopa/
 
 echo "################################ Dockerization completed ########################################################"
