3b8ec60ef07d203627a9bc5e66d5f49b45534b4b
ggopakumar <51371711+ggopakumar@users.noreply.github.com>
2019-08-16 16:06:56 +0530

    Delete dockerize.bat

--- dockerize.bat
+++
@@ -1,6 +0,0 @@
-docker build --tag=registry-ms ../registry/
-docker build --tag=product-ms ../product/
-docker build --tag=cart-ms ../CART/
-docker build --tag=etailer-ui ../Nimbus-shopping-Cart-gopa/
-
-echo "################################ Dockerization completed ########################################################"
