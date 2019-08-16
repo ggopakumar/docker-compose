c570911bd1ad67a47949ee868d52ca9ff84be653
ggopakumar <51371711+ggopakumar@users.noreply.github.com>
2019-08-16 14:43:24 +0530

    updating bat file

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 
-start java -jar ../registry-ms/target/registry-ms-0.0.1-SNAPSHOT.jar
-start java -jar ../product-ms/target/product-ms-0.0.1-SNAPSHOT.jar
-start java -jar ../cart-ms/target/cart-ms-0.0.1-SNAPSHOT.jar
-start java -jar ../etailer-ui/target/etailer-ui-0.0.1-SNAPSHOT.jar
+
+start java -jar ../registry/target/registry-ms-0.0.1-SNAPSHOT.jar
+start java -jar ../product/target/product-ms-0.0.1-SNAPSHOT.jar
+start java -jar ../CART/target/cart-ms-0.0.1-SNAPSHOT.jar
+start java -jar ../Nimbus-shopping-Cart-gopa/target/etailer-ui-0.0.1-SNAPSHOT.jar
