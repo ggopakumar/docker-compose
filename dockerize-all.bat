daa89861a2056bc806e6eecfc5bdfceb07bedce1
ggopakumar <51371711+ggopakumar@users.noreply.github.com>
2019-08-16 16:07:20 +0530

    Create dockerize-all.bat

---
+++ dockerize-all.bat
@@ -0,0 +1,6 @@
+docker build --tag=registry-ms ../registry/
+docker build --tag=product-ms ../product/
+docker build --tag=cart-ms ../CART/
+docker build --tag=etailer-ui ../Nimbus-shopping-Cart-gopa/
+
+echo "################################ Dockerization completed ########################################################"
